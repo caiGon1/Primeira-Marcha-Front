7a1e035d47ae64210b12ba0ab572f35bade07425
Caio Silva <caio.goncalvesananias@gmail.com>
2026-03-13 13:34:43 -0300

    add chat bar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,56 @@
 import { useNavigate } from "react-router-dom";
+import ChatIcon from '@mui/icons-material/Chat';
+import * as React from 'react';
+import MarkEmailUnreadIcon from '@mui/icons-material/MarkEmailUnread';
+
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 
 function Dashboard() {
   const navigator = useNavigate();
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {["Lidos"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon/> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["Não lidos"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <MarkEmailUnreadIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <div className="h-full w-full flex flex-col gap-4 p-4">
@@ -9,7 +58,10 @@ function Dashboard() {
         <button onClick={() => navigator("/")}>Logout</button>
         <button>Perfil</button>
         <button>Notificações</button>
-        <button>Chat</button>
+        <ChatIcon onClick={toggleDrawer(true)}></ChatIcon>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          {DrawerList}
+        </Drawer>
       </header>
       <div className="flex flex-row gap-10 justify-center p-3">
         <div className="h-fit w-fit">
@@ -26,7 +78,9 @@ function Dashboard() {
             </li>
           </ul>
         </div>
-        <button className="p-2 border-2 rounded w-fit h-fit">Agendar uma aula</button>
+        <button className="p-2 border-2 rounded w-fit h-fit">
+          Agendar uma aula
+        </button>
       </div>
     </div>
   );

61c34dee2601c0ca29ba9c0bbf853b8de4347cf5
Caio Silva <caio.goncalvesananias@gmail.com>
2026-03-13 10:42:33 -0300

    login não funcional

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ function Login() {
         <div className="h-screen w-screen flex items-center justify-center">
         <div className='p-4 rounded flex gap-2 w-fit flex-col items-center justify-center  border-2 '>
             <h1>Login</h1>
-            <form className="flex flex-col gap-2" action="/login" method="post" >
+            <form className="flex flex-col gap-2" action="/dashboard" method="post" >
                 <div className='grid grid-cols-[auto_auto] gap-2'>
                     <label htmlFor="email">Email:</label>
                     <input className="border-2 rounded" type="email" id="email" name="email" required />

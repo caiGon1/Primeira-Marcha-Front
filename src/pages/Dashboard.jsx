6dfa154d2924d0357d7e43c9f77cf181a8d9df0c
Caio Silva <caio.goncalvesananias@gmail.com>
2026-03-13 11:15:34 -0300

    fix dashboard css

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,27 +1,35 @@
 import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
-      const navigator = useNavigate();
+  const navigator = useNavigate();
 
-    return (<div className="h-full w-full flex flex-col gap-4 p-4">
-        <header className="flex gap-3 justify-center">
-            <button onClick={() => navigator('/')}>Logout</button>
-            <button>Perfil</button>
-            <button>Notificações</button>
-            <button>Chat</button>
-        </header>
-        <div className="grid">
-            <h1>Proximas Aulas</h1>
-            <button>Agendar uma aula</button>
-            <ul className="flex flex-col gap-2 border-2 rounded h-fit w-fit p-2">
-                <li>Aula1 <button>Cancelar</button></li>
-                <li>Aula2 <button>Cancelar</button></li>
-                <li>Aula3 <button>Cancelar</button></li>
-            </ul>
+  return (
+    <div className="h-full w-full flex flex-col gap-4 p-4">
+      <header className="flex gap-3 justify-center">
+        <button onClick={() => navigator("/")}>Logout</button>
+        <button>Perfil</button>
+        <button>Notificações</button>
+        <button>Chat</button>
+      </header>
+      <div className="flex flex-row gap-10 justify-center p-3">
+        <div className="h-fit w-fit">
+          <h1>Proximas Aulas</h1>
+          <ul className="flex flex-col gap-2 border-2 rounded p-2">
+            <li>
+              Aula1 <button>Cancelar</button>
+            </li>
+            <li>
+              Aula2 <button>Cancelar</button>
+            </li>
+            <li>
+              Aula3 <button>Cancelar</button>
+            </li>
+          </ul>
         </div>
-        <div>
-        </div>
-    </div> );
+        <button className="p-2 border-2 rounded w-fit h-fit">Agendar uma aula</button>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;

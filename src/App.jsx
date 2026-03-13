d31391abfb498442ff1773802c8366137f4068bb
Caio Silva <caio.goncalvesananias@gmail.com>
2026-03-13 10:16:49 -0300

    Fix for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./pages/DashBoard";
+import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <div>

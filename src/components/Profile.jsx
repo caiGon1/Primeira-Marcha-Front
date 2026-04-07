c41c3cd407b6b241bd05818ef482de61f9e35a5e
Caio Silva <caio.goncalvesananias@gmail.com>
2026-04-07 11:24:45 -0300

    fix to profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,15 +3,13 @@ import axios from "axios";
 import "@fontsource/inter";
 
 import MeuModal from "../components/MeuModal";
+import HideShowPassword from "./HideShowPassword";
 
 import {
   Button,
   DialogTitle,
   TextField,
-  CircularProgress,
-  Autocomplete,
-  Stack,
-  MenuItem,
+  CircularProgress
 } from "@mui/material";
 
 function Profile({ open, onClose }) {
@@ -19,19 +17,13 @@ function Profile({ open, onClose }) {
 
   const [nomePerfilOpen, setNomePerfilOpen] = useState(false);
   const [emailPerfilOpen, setEmailPerfilOpen] = useState(false);
-  const [cidadePerfilOpen, setCidadePerfilOpen] = useState(false);
-  const [ufPerfilOpen, setUfPerfilOpen] = useState(false);
+  const [senhaPerfilOpen, setSenhaPerfilOpen] = useState(false);
 
   const [loading, setLoading] = useState(false);
 
-  const [cidades, setCidades] = useState([]);
-  const [cidadesUf, setCidadesUf] = useState([]);
-  const [ufs, setUFs] = useState([]);
-
   const [novoNome, setNovoNome] = useState("");
   const [novoEmail, setNovoEmail] = useState("");
-  const [novaCidade, setNovaCidade] = useState("");
-  const [novaUF, setNovaUF] = useState("");
+  const [novaSenha, setNovaSenha] = useState("");
 
 
   useEffect(() => {
@@ -57,31 +49,7 @@ function Profile({ open, onClose }) {
   }, []);
 
 
-  useEffect(() => {
-    if (!user?.UF) return;
 
-    fetch(
-      `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${user.UF}/municipios`
-    )
-      .then((res) => res.json())
-      .then(setCidades);
-  }, [user]);
-
-  useEffect(() => {
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-      .then((res) => res.json())
-      .then(setUFs);
-  }, []);
-
-  useEffect(() => {
-    if (!novaUF) return;
-
-    fetch(
-      `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${novaUF}/municipios`
-    )
-      .then((res) => res.json())
-      .then(setCidadesUf);
-  }, [novaUF]);
 
 
 
@@ -127,12 +95,8 @@ function Profile({ open, onClose }) {
           Alterar email
         </Button>
 
-        <Button onClick={() => setCidadePerfilOpen(true)}>
-          Alterar cidade
-        </Button>
-
-        <Button onClick={() => setUfPerfilOpen(true)}>
-          Alterar UF
+        <Button onClick={() => setSenhaPerfilOpen(true)}>
+          Alterar senha
         </Button>
       </MeuModal>
 
@@ -179,68 +143,20 @@ function Profile({ open, onClose }) {
         </form>
       </MeuModal>
 
-      <MeuModal
-        open={cidadePerfilOpen}
-        onClose={() => setCidadePerfilOpen(false)}
-      >
-        <DialogTitle>Alterar cidade</DialogTitle>
+      <MeuModal open={senhaPerfilOpen} onClose={() => setSenhaPerfilOpen(false)}>
+        <DialogTitle>Alterar senha</DialogTitle>
 
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            updateUser({ cidade: novaCidade });
+            updateUser({ senha: novaSenha });
           }}
         >
-          <Autocomplete
-            options={cidades.map((c) => c.nome)}
-            value={novaCidade}
-            onChange={(e, value) => setNovaCidade(value)}
-            renderInput={(params) => (
-              <TextField {...params} label="Cidade" />
-            )}
+          <HideShowPassword
+            value={novaSenha}
+            onChange={(e) => setNovaSenha(e.target.value)}
+            fullWidth
           />
-
-          <Button type="submit">
-            {loading ? <CircularProgress size={20} /> : "Salvar"}
-          </Button>
-        </form>
-      </MeuModal>
-
-
-      <MeuModal open={ufPerfilOpen} onClose={() => setUfPerfilOpen(false)}>
-        <DialogTitle>Alterar UF</DialogTitle>
-
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            updateUser({
-              UF: novaUF,
-              cidade: novaCidade,
-            });
-          }}
-        >
-          <Stack gap={2}>
-            <TextField
-              select
-              value={novaUF}
-              onChange={(e) => setNovaUF(e.target.value)}
-            >
-              {ufs.map((uf) => (
-                <MenuItem key={uf.sigla} value={uf.sigla}>
-                  {uf.sigla}
-                </MenuItem>
-              ))}
-            </TextField>
-
-            <Autocomplete
-              disabled={!novaUF}
-              options={cidadesUf.map((c) => c.nome)}
-              onChange={(e, value) => setNovaCidade(value)}
-              renderInput={(params) => (
-                <TextField {...params} label="Cidade" />
-              )}
-            />
-          </Stack>
 
           <Button type="submit">
             {loading ? <CircularProgress size={20} /> : "Salvar"}
